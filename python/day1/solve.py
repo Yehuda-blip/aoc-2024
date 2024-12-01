bd5649ba42374262f3579bb0854d095eab85c1a0
Yehuda-blip <yehudak12321@gmail.com>
2024-12-01 23:55:04 +0200

    asdf

--- python/day1/solve.py
+++ python/day1/solve.py
@@ -2,6 +2,8 @@ from collections import Counter
 import itertools
 from typing import Iterator
 
+import numpy as np
+
 
 EXAMPLE = """\
 3   4
@@ -17,31 +19,23 @@ with open("inputs\day1.txt", 'r') as text:
     INPUT = text.read()
 
 
-def parse(line: str) -> tuple[int, int]:
-    a, b = line.split()
-    return int(a), int(b)
-
-
-def lists(data: Iterator[tuple[int, int]]) -> tuple[list[int], list[int]]:
-    data1, data2 = itertools.tee(data)
-    data1, data2 = map(lambda r: r[0], data1), map(lambda r: r[1], data2)
-    return sorted(data1), sorted(data2)
+def parse(text: str) -> np.ndarray[int]:
+    parseline = lambda line: list(map(int, line.split()))
+    return np.array(list(map(parseline, text.splitlines())))
 
 
 def solve_a(text: str) -> int:
-    data1, data2 = lists(map(parse, text.splitlines()))
-    data = zip(data1, data2)
-    result = sum(map(lambda row: abs(row[0] - row[1]), data))
-    return result
+    data1, data2 = np.sort(parse(text).T)
+    return int(np.sum(np.abs(data1 - data2)))
 
 
 print(f"{solve_a(EXAMPLE)=}, {solve_a(INPUT)=}")
 
 
 def solve_b(text: str) -> int:
-    data1, data2 = lists(map(parse, text.splitlines()))
+    data1, data2 = parse(text).T
     count = Counter(data2)
-    return sum(count[v] * v for v in data1)
+    return int(sum(count[v] * v for v in data1))
 
 
 print(f"{solve_b(EXAMPLE)=}, {solve_b(INPUT)=}")
